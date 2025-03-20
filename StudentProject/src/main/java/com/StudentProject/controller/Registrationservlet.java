f5e8016467615ed3424c51e0c5e24c39d7142f8d
Akhil <Akhil@Akhil_Maddela>
2025-03-20 13:36:45 +0530

    Changes in Controller

--- StudentProject/src/main/java/com/StudentProject/controller/Registrationservlet.java
+++ StudentProject/src/main/java/com/StudentProject/controller/Registrationservlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.StudentProject.bean.StudentBean;
+
 import com.StudentProject.dao.StudentDao;
 
 /**
@@ -19,19 +20,21 @@ import com.StudentProject.dao.StudentDao;
 @WebServlet("/registrationservlet")
 public class Registrationservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Registrationservlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Registrationservlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		@SuppressWarnings("unused")
 		String Name = request.getParameter("Name");
 		@SuppressWarnings("unused")
@@ -45,45 +48,44 @@ public class Registrationservlet extends HttpServlet {
 		@SuppressWarnings("unused")
 		String College = request.getParameter("College");
 
-		StudentBean s = new StudentBean(); //As we need to use across the project creating the
+		StudentBean s = new StudentBean(); // As we need to use across the project creating the
 
 		s.setName("Name");
 		s.setMobileno("Mobileno");
-		s.setAddress ("Address");
+		s.setAddress("Address");
 		s.setEmail("Email");
 		s.setTech("Tech");
 		s.setCollege("College");
-		
+
 		@SuppressWarnings("unused")
+
 		StudentDao rd = new StudentDao();
-		int result = StudentDao.Insertdata(s); //we will have value for result
+		int result = StudentDao.Insertdata(s); // we will have value for result
+
 		HttpSession session = request.getSession(); // keeping the session open
-		(session).setAttribute("StudentObject",s);
-		
+		(session).setAttribute("StudentObject", s);
+
 		RequestDispatcher Success = request.getRequestDispatcher("Success.html");
 		RequestDispatcher Fail = request.getRequestDispatcher("Fail.html");
-		
 
-		if(result>0) {
+		if (result > 0) {
 			Success.forward(request, response);
-		}
-		else {
+		} else {
 			Fail.forward(request, response);
 		}
-
 
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 		doGet(request, response);
 	}
-	
-	
 
 }
